74ceb6007bf96f68283f32427210c680a8bd28be
architsangal <architsangal2000@gmail.com>
2023-06-16 11:50:01 +0530

    made some changes

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Portfolio
+
+This is my portfolio website. It is built with React and hosted on GitHub Pages.
+
+		// "start": "react-scripts --openssl-legacy-provider start",
+https://www.youtube.com/watch?v=Q9n2mLqXFpU
--- src/App.js
+++ src/App.js
@@ -23,8 +23,8 @@ function App() {
 					<AppHeader />
 					<Suspense fallback={""}>
 						<Routes>
-							<Route path="/" element={<Home />} />
-							<Route path="projects" element={<Projects />} />
+							<Route path="/architsangal-portfolio/" element={<Home />} />
+							<Route path="/architsangal-portfolio/projects" element={<Projects />} />
 							<Route
 								path="projects/single-project"
 								element={<ProjectSingle />}
